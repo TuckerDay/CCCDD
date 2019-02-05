d2f7f7c192b034cef6ab8587fc5aa663ab456be8
R-Dawg <raleigh.dial@gmail.com>
2019-02-05 14:56:29 -0700

    Raleigh is here
    I have entered

--- zybooks/ch6/group.c
+++ zybooks/ch6/group.c
@@ -15,10 +15,15 @@ void tucker() {
 
 }
 
-void raleigh() {
-
+void raleigh(){
+    printf("Hi I'm Raleigh.\n");
+    //    printf("I am a Sophmore.\n");
+    //    printf("Unsurprisingly I am a CS major.\n");
+    //    printf("I enjoy CS and complaining.\n");
+    //    printf("I grew up in ATL, GA.\n");
+    //    printf("I am not interesting.\n\n");
 }
 
 void main() {
-
+    raleigh();
 }
