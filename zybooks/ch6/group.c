ebec637f5d14e200a56bb1deddb8d9a150b7fbfe
connercopeland <>
2019-02-05 15:13:14 -0700

    Added hobbies

--- zybooks/ch6/group.c
+++ zybooks/ch6/group.c
@@ -3,6 +3,7 @@ void conner(){
     printf("Name: Conner Copeland\n");
     printf("Year: Senior\n");
     printf("Major: Computer Science\n");
+    printf("Hobbies: I like playing video games, reading, and a bit of birding\n");
 }
 
 void parker() {
