0948ed684e7c63e76b9a4c35bcdbd67dc4471934
[Nicolas Crepeau] <[ncrepeau@icloud.com]>
2019-02-05 14:58:35 -0700

    Update group.c
    Added 2nd line

--- zybooks/ch6/group.c
+++ zybooks/ch6/group.c
@@ -8,7 +8,11 @@ void parker() {
 
 void nicolas() {
 printf("My name is: Nicolas Crepeau");
-
+printf("I am a freshman");
+//printf("I am a computer science major");
+//printf("My hobbies include: baseball and C & C++");
+//printf("I grew up in Brooklyn");
+//printf("There is nothing else that is interesting about my life outside of these 4 prior questions");
 }
 
 void tucker() {
