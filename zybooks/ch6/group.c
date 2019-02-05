a4fa0a350d04d914eb7e83b399b6b22963505677
R-Dawg <raleigh.dial@gmail.com>
2019-02-05 14:59:48 -0700

    Added hobby
    He is really boring

--- zybooks/ch6/group.c
+++ zybooks/ch6/group.c
@@ -19,7 +19,7 @@ void raleigh(){
     printf("Hi I'm Raleigh.\n");
     printf("I am a Sophmore.\n");
     printf("Unsurprisingly I am a CS major.\n");
-    //    printf("I enjoy CS and complaining.\n");
+    printf("I enjoy CS and complaining.\n");
     //    printf("I grew up in ATL, GA.\n");
     //    printf("I am not interesting.\n\n");
 }
