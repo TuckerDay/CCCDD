d5047d5a805d4c3c30c48805954506130ec0f1cc
R-Dawg <raleigh.dial@gmail.com>
2019-02-05 14:59:29 -0700

    Added major
    His major is CS

--- zybooks/ch6/group.c
+++ zybooks/ch6/group.c
@@ -18,7 +18,7 @@ void tucker() {
 void raleigh(){
     printf("Hi I'm Raleigh.\n");
     printf("I am a Sophmore.\n");
-    //    printf("Unsurprisingly I am a CS major.\n");
+    printf("Unsurprisingly I am a CS major.\n");
     //    printf("I enjoy CS and complaining.\n");
     //    printf("I grew up in ATL, GA.\n");
     //    printf("I am not interesting.\n\n");
